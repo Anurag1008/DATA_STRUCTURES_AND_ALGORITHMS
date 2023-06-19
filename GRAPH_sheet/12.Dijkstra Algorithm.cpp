19724a397430030343e556f39ae2af822e645a42
Anurag1008 <julyanurag@gmail.com>
2023-06-19 20:19:36 +0530

    Dijkstra Algorithm

---
+++ GRAPH_sheet/12.Dijkstra Algorithm.cpp
@@ -0,0 +1,41 @@
+//Dijkstra Algorithm
+
+/*
+class Solution
+{
+	public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    int findminNode(vector<int>&dist,vector<bool>&checkNode)
+    {
+        int Min = INT_MAX; int MinIndex;
+        for(int v=0;v<dist.size();v++){
+            if(!checkNode[v] && dist[v]<=Min){
+                Min = dist[v];
+                MinIndex = v; 
+            }
+        }
+        return MinIndex;
+    }
+    
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        vector<bool>checkNode(V,false);
+        vector<int>dist(V,INT_MAX);
+        
+        dist[S]=0;
+        
+        for(int i=0;i<V-1;i++){
+            int u = findminNode(dist,checkNode);
+            checkNode[u] = true;
+            for(auto neighbors : adj[u]){
+                int w = neighbors[1];
+                int node = neighbors[0];
+                if(!checkNode[node] && dist[u]!=INT_MAX && dist[u]+w < dist[node])
+                    dist[node] = dist[u]+w;
+            }
+        }
+        return dist;
+    }
+};
+*/
