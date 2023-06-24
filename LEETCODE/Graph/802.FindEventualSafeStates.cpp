36cee8fe1f55d8e44cdf6b41b4501d25b3fa03c5
Anurag1008 <julyanurag@gmail.com>
2023-06-25 05:20:30 +0530

    Find Eventual Safe States

---
+++ LEETCODE/Graph/802.FindEventualSafeStates.cpp
@@ -0,0 +1,47 @@
+//Find Eventual Safe States
+
+//concecpt of indegree and outdegree
+
+/*
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        //lets create a outdegree array first.
+        int n = graph.size();
+        vector<int>outdegree(n,0);
+        for(int i=0;i<graph.size();i++) outdegree[i] = graph[i].size();
+
+        //now change the graph to know with whom outdegree graph with 0 outdegree are connected.
+        vector<vector<int>>inDegree(n);
+
+        for(int i=0;i<n;i++)
+            for(auto node : graph[i]) 
+                    if(node != i) inDegree[node].push_back(i);
+        
+        cout<<endl;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<inDegree[i].size();j++){
+                cout<<inDegree[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        
+        vector<int>ans;
+        queue<int>q;
+        for(int i=0;i<n;i++) if(outdegree[i] == 0) q.push(i);
+          
+        while(!q.empty()){
+            int front = q.front();
+            ans.push_back(front);
+            q.pop();
+            for(auto neighbor : inDegree[front]){
+                outdegree[neighbor]--;
+                if(outdegree[neighbor] == 0) q.push(neighbor);
+            }
+        }
+         
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
+*/
