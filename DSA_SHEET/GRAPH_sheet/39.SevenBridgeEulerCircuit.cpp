4647f679a3ac0e9ef8d5be7bf4da426264b94914
Anurag1008 <julyanurag@gmail.com>
2023-06-24 19:30:26 +0530

    Euler circuit and Path

---
+++ DSA_SHEET/GRAPH_sheet/39.SevenBridgeEulerCircuit.cpp
@@ -0,0 +1,15 @@
+//Euler circuit and Path
+
+/*
+class Solution {
+public:
+	int isEularCircuit(int V, vector<int>adj[]){
+	    int odd = 0;
+	    for(int i=0;i<V;i++)
+	        if(adj[i].size() & 1) odd++;
+	    if(odd == 0) return 2;
+	    else if (odd == 2) return 1;
+	    else return 0;
+	}
+};
+*/
