04658509fbbc5e0daa17a1065c0ba0111754be7f
Anurag1008 <julyanurag@gmail.com>
2023-06-26 23:02:43 +0530

    practice of maxheap

---
+++ Class/PriorityQueue/practice.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+int size = 0;
+void swap(int *a,int *b){
+    int temp = *b;
+    *b = *a;
+    *a = temp;
+}
+
+void heapify(int arr[],int size,int i){
+    if(size == 0) printf("\n contain a single element \n");
+    else{
+        int largest = i;
+        int l = 2*i+1;
+        int r = 2*i+2;
+        //main purpose it is not greater than the right.
+        if(l < size && arr[l] > arr[largest]) largest = l;
+        if(r < size && arr[r] > arr[largest]) largest = r;
+        if(largest != i){
+            swap(&arr[i],&arr[largest]);
+            heapify(arr,size,largest);
+        }
+    }
+}
+
+void insert(int arr[],int data){
+    if(size == 0){
+        arr[0] = data;
+        size++;
+    }
+    else{
+        arr[size] = data;
+        size++;
+        for(int i=size/2+1;i>=0;i--){
+            heapify(arr,size,i);
+        }
+    }
+}
+
+void delete(int arr[],int data){
+    if(size == 0) printf("priorityQueue is empty");
+    else{
+        for(int i=0;i<size;i++){
+            if(arr[i] == data){
+                swap(&arr[i],&arr[size-1]);
+                size--;
+                for(int i=size/2-1;i>=0;i--){
+                    heapify(arr,size,i);
+                }
+            }
+        }
+    }
+}
+
+void print(int arr[]){
+    for(int i=0;i<size;i++){
+        printf("%d ",arr[i]);
+    }
+}
+
+int main(){
+    int arr[100];
+    insert(arr,5);
+    insert(arr,7);
+    insert(arr,19);
+    insert(arr,11);
+    insert(arr,12);
+    delete(arr,19);
+    delete(arr,11);
+    print(arr);
+    return 0;
+}
--- Class/PriorityQueue/priorityQueue.c
+++ Class/PriorityQueue/priorityQueue.c
@@ -2,17 +2,22 @@
 
 #include <stdio.h>
 int size = 0;
-void swap(int *a, int *b) {
+void swap(int *a, int *b)
+{
   int temp = *b;
   *b = *a;
   *a = temp;
 }
 
 // Function to heapify the tree
-void heapify(int array[], int size, int i) {
-  if (size == 1) {
+void heapify(int array[], int size, int i)
+{
+  if (size == 1)
+  {
     printf("Single element in the heap");
-  } else {
+  }
+  else
+  {
     // Find the largest among root, left child and right child
     int largest = i;
     int l = 2 * i + 1;
@@ -23,7 +28,8 @@ void heapify(int array[], int size, int i) {
       largest = r;
 
     // Swap and continue heapifying if root is not largest
-    if (largest != i) {
+    if (largest != i)
+    {
       swap(&array[i], &array[largest]);
       heapify(array, size, largest);
     }
@@ -31,58 +37,63 @@ void heapify(int array[], int size, int i) {
 }
 
 // Function to insert an element into the tree
-void insert(int array[], int newNum) {
-  if (size == 0) {
+void insert(int array[], int newNum)
+{
+  if (size == 0)
+  {
     array[0] = newNum;
     size += 1;
-  } else {
+  }
+  else
+  {
     array[size] = newNum;
     size += 1;
-    for (int i = size / 2 - 1; i >= 0; i--) {
+    for (int i = size / 2 - 1; i >= 0; i--)
+    {
       heapify(array, size, i);
     }
   }
 }
 
 // Function to delete an element from the tree
-void deleteRoot(int array[], int num) {
+void deleteRoot(int array[], int num)
+{
   int i;
-  for (i = 0; i < size; i++) {
+  for (i = 0; i < size; i++)
+  {
     if (num == array[i])
       break;
   }
 
   swap(&array[i], &array[size - 1]);
   size -= 1;
-  for (int i = size / 2 - 1; i >= 0; i--) {
+  for (int i = size / 2 - 1; i >= 0; i--)
+  {
     heapify(array, size, i);
   }
 }
 
 // Print the array
-void printArray(int array[], int size) {
+void printArray(int array[], int size)
+{
   for (int i = 0; i < size; ++i)
     printf("%d ", array[i]);
   printf("\n");
 }
 
 // Driver code
-int main() {
+int main()
+{
   int array[10];
 
-  insert(array, 3);
-  insert(array, 4);
-  insert(array, 9);
   insert(array, 5);
-  insert(array, 2);
+  insert(array, 7);
+  insert(array, 19);
+  insert(array, 11);
+  insert(array, 12);
 
   printf("Max-Heap array: ");
   printArray(array, size);
-
-  deleteRoot(array, 4);
-  deleteRoot(array,9);
-
-  printf("After deleting an element: ");
 
   printArray(array, size);
 }
