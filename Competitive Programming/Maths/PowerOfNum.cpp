3b0062e5401cac469045bf82ba41d7023482684c
Anurag1008 <julyanurag@gmail.com>
2023-07-01 18:42:19 +0530

    PowerOfNum

---
+++ Competitive Programming/Maths/PowerOfNum.cpp
@@ -0,0 +1,36 @@
+//50. Pow(x, n)
+
+/*
+class Solution {
+public:
+    double pow(double x,int n){
+        if(n == 0) return 1;
+        double y  = myPow(x,n/2);
+        if(n < 0) return 1/y;
+        if(n % 2 == 0) return y*y;
+        else return y*y*x;
+    }
+    double myPow(double x, int n) {
+        double ans = pow(x,abs(n));
+        if(n >= 0) return ans;
+        else return 1/ans;
+    }
+};
+*/
+
+/*
+class Solution {
+public: 
+    //Iterative solution by using Bits
+    double myPow(double x, int n) {
+        int pow = abs(n);
+        double ans = 1;
+        while(pow > 0){
+            if(pow % 2 == 1) ans *= x;
+            pow>>=1;
+            x*=x;
+        }
+        return n < 0 ? 1/ans : ans;
+    }
+};
+*/
