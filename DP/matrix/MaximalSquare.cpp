d18b6fc40b85ea4ad3b1b5e38d4e2855548bf57a
Anurag1008 <julyanurag@gmail.com>
2023-07-03 22:25:14 +0530

    221. Maximal Square

---
+++ DP/matrix/MaximalSquare.cpp
@@ -0,0 +1,75 @@
+//221. Maximal Square
+
+#include<iostream>
+#include<vector>
+#include<stack>
+#include <limits.h>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int>nextSmaller(vector<int>&v){
+        int n = v.size();
+        vector<int>ans(n);
+        stack<int>s;
+        s.push(-1);
+        for(int i=n-1;i>=0;i--){
+            int curr = v[i];
+            while(s.top()!=-1 && v[s.top()] >= curr) s.pop();
+            ans[i] = s.top();
+            s.push(i);
+        }
+        return ans;
+    }
+
+    vector<int>prevSmaller(vector<int>&v){
+        int n = v.size();
+        vector<int>ans(n);
+        stack<int>s;
+        s.push(-1);
+        for(int i=0;i<n;i++){
+            int curr = v[i];
+            while(s.top()!=-1 && v[s.top()] >= curr) s.pop();
+            ans[i] = s.top();
+            s.push(i);
+        }
+        return ans;
+    }
+
+    int LargestSquareArea(vector<int>&height){
+        int n = height.size();
+
+        vector<int>next(n);
+        next = nextSmaller(height);
+
+        vector<int>prev(n);
+        prev = prevSmaller(height);
+        
+        int ans = INT_MIN;
+        for(int i=0;i<n;i++){
+            int l = height[i];
+            if(next[i] == -1) next[i] = n;
+            int b = next[i] - prev[i] - 1;
+            int side = min(l,b);
+            int area = side*side;
+            ans = max(ans,area);
+        }
+        return ans;
+    }
+
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int rows = matrix.size();
+        int n = matrix[0].size();
+        vector<int>histogram(n,0);
+        int ans  = INT_MIN;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<matrix[i].size();j++){
+                if(matrix[i][j] == '1') histogram[j]++;
+                else histogram[j] = 0;
+            }
+            ans = max(ans,LargestSquareArea(histogram));
+        }
+        return ans;
+    }
+};
