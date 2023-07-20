a252e7f86a28d54bfd9abdd84989d140d9ecf1af
Anurag1008 <julyanurag@gmail.com>
2023-07-20 22:16:36 +0530

    .

---
+++ DSA_SHEET/BIT_MASK_sheet/4.Count_Total_Setbits.cpp
@@ -0,0 +1,23 @@
+//https://practice.geeksforgeeks.org/problems/sherlock-and-his-enemies2304/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+
+/*
+//TLE
+class Solution{
+public:
+    int countBits(int N){
+        int count  = 0;
+        for(int i=1;i<=N;i++){
+            int a = i;
+            while(a!=0){
+                if((a&1) == 1) count++;
+                a>>=1;
+            }
+        }
+        return count;
+    }
+};
+
+
+
+
+*/
