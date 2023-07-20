7ae3cb6f834ef6cf81bfb44c3e0cf43a96c7bd2b
Anurag1008 <julyanurag@gmail.com>
2023-07-20 16:45:13 +0530

    Count Number of set Bit

---
+++ DSA_SHEET/BIT_MASK_sheet/CountSetBit.cpp
@@ -0,0 +1,17 @@
+//https://practice.geeksforgeeks.org/problems/set-bits0143/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+
+//Number of 1 Bits
+
+/*
+class Solution {
+  public:
+    int setBits(int N) {
+        int count = 0;
+        while(N != 0){
+            if(N&1 == 1) count++;
+            N>>=1;
+        }
+        return count;
+    }
+};
+*/
