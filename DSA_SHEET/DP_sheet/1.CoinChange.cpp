4cf35434f2ce323b933ec109b545c2068ea61d98
Anurag1008 <julyanurag@gmail.com>
2023-08-03 22:20:45 +0530

    coin change

--- DSA_SHEET/DP_sheet/1.CoinChange.cpp
+++ DSA_SHEET/DP_sheet/1.CoinChange.cpp
@@ -1,7 +1,116 @@
 
+//https://leetcode.com/problems/coin-change/description/
 //https://practice.geeksforgeeks.org/explore?page=1&sortBy=submissions
 
-// Coin Change
+/*
+
+322. Coin Change
+
+//recursion
+
+class Solution {
+public:
+    int solve(vector<int>&coins,int amount){
+        if(amount == 0) return 0;
+        if(amount < 0) return INT_MAX;
+        int mini = INT_MAX;
+        for(int i=0;i<coins.size();i++){
+            int ans = solve(coins,amount - coins[i]);
+            if(ans!= INT_MAX) 
+                mini = min(mini,ans+1);
+        }
+        return mini;
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int ans = solve(coins,amount);
+        if(ans == INT_MAX) return -1;
+        return ans;
+    }
+};
+
+//taken notTaken Recursion
+
+class Solution {
+public:
+    //its  a case of taken or not taken 
+    int solve(vector<int>& coins,int index,int amount)
+    {
+        if(index >= coins.size() || amount <= 0)
+            return (amount == 0) ? 0 : INT_MAX - 1;
+        
+        int ans  = -1;
+        if(coins[index] > amount)
+        {
+            int notTaken = 0 + solve(coins,index+1,amount);
+            ans = notTaken;
+        }
+        else
+        {
+            int takeCoin = 1 + solve(coins,index + 0,amount - coins[index]);
+            int notTaken = 0 + solve(coins,index + 1,amount);
+            ans = min(takeCoin,notTaken);
+        }
+        return ans;
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int val = solve(coins,0,amount);
+        return (val == INT_MAX - 1) ? -1 : val;
+    }
+};
+
+//Memorization
+class Solution {
+public:
+    int solve(vector<int>&coins,int amount,vector<int>&dp){
+        if(amount == 0) return 0;
+        if(amount < 0) return INT_MAX;
+        if(dp[amount] != -1) return dp[amount];
+        int mini = INT_MAX;
+        for(int i=0;i<coins.size();i++){
+            int ans = solve(coins,amount - coins[i],dp);
+            if(ans!= INT_MAX) 
+                mini = min(mini,ans+1);
+        }
+        dp[amount] = mini;
+        return mini;
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<int>dp(amount+1,-1);
+        int ans = solve(coins,amount,dp);
+        if(ans == INT_MAX) return -1;
+        return ans;
+    }
+};
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 2.Coin Change
 
 /*
 
