735f62537029555727169d91de740d97989345eb
Anurag1008 <julyanurag@gmail.com>
2023-08-11 18:40:33 +0530

    practice

--- DSA_SHEET/DP_sheet/24.MaxLengthOfChain.cpp
+++ DSA_SHEET/DP_sheet/24.MaxLengthOfChain.cpp
@@ -26,4 +26,19 @@ public:
         return recc(0,-1,pairs,dp);
     }
 };
+
+//Tabulation
+
+int findLongestChain(vector<vector<int>>& pairs) {
+        sort(pairs.begin(),pairs.end());
+        int maxi = 1;
+        const int n = pairs.size();
+        vector<int> dp(n,1);
+        for(int i=0;i<n;++i){
+            for(int prev=0;prev<i;++prev){
+                if((pairs[i][0]>pairs[prev][1]) && (dp[i]<1+dp[prev])){dp[i] = 1 + dp[prev];}
+            }
+            maxi = max(maxi,dp[i]);
+        }
+        return maxi;
 */
