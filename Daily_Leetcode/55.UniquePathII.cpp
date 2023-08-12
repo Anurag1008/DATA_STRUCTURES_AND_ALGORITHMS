1d582413488b68c1523c3fc758471e0c581ffa3b
Anurag1008 <julyanurag@gmail.com>
2023-08-12 22:49:43 +0530

    unique path 2

---
+++ Daily_Leetcode/55.UniquePathII.cpp
@@ -0,0 +1,26 @@
+//63. Unique Paths II
+
+/*
+class Solution {
+public:
+    int uniquePaths(int x,int y,vector<vector<int>>& grid,vector<vector<int>>&dp){
+        int n = grid.size();
+        int m = grid[0].size();
+        if(x < 0 || y < 0 || x >=n || y >= m) return 0;
+        if(grid[x][y] == 1) return 0;
+        if(x == n-1 && y == m-1 ) return 1;
+
+        if(dp[x][y] != -1) return dp[x][y];
+        
+        dp[x][y] = uniquePaths(x+1,y,grid,dp) + uniquePaths(x,y+1,grid,dp);
+        return dp[x][y];
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        //obstacle is represented by 1
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return uniquePaths(0,0,obstacleGrid,dp);
+    }
+};
+*/
