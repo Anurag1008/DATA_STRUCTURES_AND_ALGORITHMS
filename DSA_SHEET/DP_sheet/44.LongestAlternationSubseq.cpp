217a323bb1bf70d7115f68d1c5dd2e7f31a394ce
Anurag1008 <julyanurag@gmail.com>
2023-08-17 20:40:48 +0530

    Longest Alternating Subseq

---
+++ DSA_SHEET/DP_sheet/44.LongestAlternationSubseq.cpp
@@ -0,0 +1,21 @@
+//https://practice.geeksforgeeks.org/problems/longest-alternating-subsequence5951/1
+
+//Longest Alternating Subsequence
+
+/*
+class Solution {
+	public:
+		int AlternatingaMaxLength(vector<int>&nums){
+		    int n = nums.size();
+		    if(n<2) return 1;
+		    int inc = 1;
+		    int dec = 1;
+		    for(int i=1;i<n;i++){
+		        if(nums[i] > nums[i-1]) inc = dec+1;
+		        else if(nums[i] < nums[i-1]) dec = inc+1;
+		    }
+		    return max(inc,dec);
+		}
+
+};
+*/
