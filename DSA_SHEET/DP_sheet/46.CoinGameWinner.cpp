c9130a559a84f5a1b287df7603e372af90d70d15
Anurag1008 <julyanurag@gmail.com>
2023-08-18 20:19:47 +0530

    Practice

---
+++ DSA_SHEET/DP_sheet/46.CoinGameWinner.cpp
@@ -0,0 +1,26 @@
+
+//https://practice.geeksforgeeks.org/problems/geek-and-its-game-of-coins4043/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+
+//Coin game winner where every player has three choices
+
+/*
+
+class Solution{
+
+	public:
+	int findWinner(int N, int X, int Y)
+	{
+		bool dp[N+1] = {false};
+		dp[0] = false;
+		dp[1] = true;
+		for(int i=2;i<=N;i++){
+		    if( i-1 >= 0 && !dp[i-1] ) dp[i] = true;
+		    else if( i-X >= 0 && !dp[i-X]  ) dp[i] = true;
+		    else if( i-Y >= 0 && !dp[i-Y]) dp[i] = true;
+		    else dp[i] = false;
+		}
+		return dp[N];
+	}
+
+};
+*/
