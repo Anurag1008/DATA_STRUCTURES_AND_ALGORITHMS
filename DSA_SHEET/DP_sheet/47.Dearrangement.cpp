670112b10020294a522197fb3fd271cc063e9b0c
Anurag1008 <julyanurag@gmail.com>
2023-08-18 20:36:14 +0530

    Count number of Dearranngement

---
+++ DSA_SHEET/DP_sheet/47.Dearrangement.cpp
@@ -0,0 +1,9 @@
+
+//https://www.codingninjas.com/studio/problems/count-derangements_873861
+
+//Count number of Dearrangement 
+
+
+//So,that no elements appear in their original place.
+
+//Recurrsive_Relation (N-1)*(Recc(N-1)*Recc(N-2))
