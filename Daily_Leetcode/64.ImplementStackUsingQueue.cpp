edf7b0c6bb0754fd8ff100000c91ec15228af518
Anurag1008 <julyanurag@gmail.com>
2023-08-29 17:20:35 +0530

    changes

--- Daily_Leetcode/64.ImplementStackUsingQueue.cpp
+++ Daily_Leetcode/64.ImplementStackUsingQueue.cpp
@@ -39,13 +39,4 @@ public:
         
     }
 };
-
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack* obj = new MyStack();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->top();
- * bool param_4 = obj->empty();
- */
 */
