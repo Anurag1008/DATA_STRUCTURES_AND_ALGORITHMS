d5e4da719522964c08d77ab8824c0896ef10f17e
Anurag1008 <julyanurag@gmail.com>
2023-09-01 16:11:56 +0530

    optamized solution

--- Daily_Leetcode/68.cpp
+++ Daily_Leetcode/68.cpp
@@ -24,5 +24,17 @@ public:
 */
 /*
 
+//more optamized answer
 
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>t(n+1);
+        t[0] = 0;
+        for(int i = 1;i<=n;++i)
+            t[i] = t[i/2] + i%2;
+        
+        return t;
+    }
+};
 */
