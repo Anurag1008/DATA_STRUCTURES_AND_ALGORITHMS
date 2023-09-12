e0b127750e8ba53cb5fa3852f6e3946c25a38789
Anurag1008 <julyanurag@gmail.com>
2023-09-12 20:20:53 +0530

    practice

--- DSA_SHEET/DP_sheet/2.KnapSackProblem.cpp
+++ DSA_SHEET/DP_sheet/2.KnapSackProblem.cpp
@@ -19,3 +19,33 @@ class Solution
     }
 };
 */
+
+
+//Memorization
+/*
+class Solution
+{
+    public:
+    //Function to return max value that can be put in knapsack of capacity W.
+    int solve(int idx,int W,int wt[],int val[],int n,vector<vector<int>>&dp){
+        if(idx < 0 || W == 0) return 0;
+        if(dp[idx][W]!= -1) return dp[idx][W];
+        if(wt[idx] > W) return solve(idx-1,W,wt,val,n,dp);
+        int ans = 0;
+        int taken = val[idx] + solve(idx-1,W-wt[idx],wt,val,n,dp);
+        int notTaken =solve(idx-1,W,wt,val,n,dp);
+        return dp[idx][W] =max(taken,notTaken);
+    }
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       vector<vector<int>>dp(n,vector<int>(W+1,-1));
+       return solve(n-1,W,wt,val,n,dp);
+    }
+};
+*/
+
+//Tabulatoion
+/*
+
+
+*/
