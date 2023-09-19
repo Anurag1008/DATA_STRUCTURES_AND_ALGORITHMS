c4ec48f12be315f2efbde1d8275396b39ce4c5ef
Anurag1008 <julyanurag@gmail.com>
2023-09-19 22:39:12 +0530

    practice

---
+++ Daily_Leetcode/76.cpp
@@ -0,0 +1,22 @@
+/*
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        //fast slow approach
+        int slow= 0;
+        int fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        return slow;
+    }
+};
+*/
