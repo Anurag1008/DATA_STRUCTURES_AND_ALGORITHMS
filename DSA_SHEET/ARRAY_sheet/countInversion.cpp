6b053ff4debf9a062fdd2166b7b7cbee01856d3e
Anurag1008 <julyanurag@gmail.com>
2023-09-21 21:33:26 +0530

    pratuce

--- DSA_SHEET/ARRAY_sheet/countInversion.cpp
+++ DSA_SHEET/ARRAY_sheet/countInversion.cpp
@@ -1,57 +1,23 @@
-//315. Count of Smaller Numbers After Self
-//Leetcode
-
-/*
-#include <vector>
-using namespace std;
-
-class Solution {
-public:
-    vector<int> countSmallerSegmentTree(vector<int>& nums) {
-        int num_buckets = 2 * 10000 + 1;
-        vector<int> segment_tree(4 * num_buckets, 0);
-        vector<int> result(nums.size());
-
-        function<void(int, int, int, int)> update = [&](int tree_index, int bucket, int tree_left, int tree_right) {
-            if (tree_left == tree_right) {
-                segment_tree[tree_index]++;
-                return;
-            }
-
-            int m = (tree_left + tree_right) / 2;
-            if (bucket <= m) {
-                update(2 * tree_index + 1, bucket, tree_left, m);
-            } else if (bucket > m) {
-                update(2 * tree_index + 2, bucket, m + 1, tree_right);
-            }
-
-            segment_tree[tree_index]++;
-        };
-
-        function<int(int, int, int, int)> query_range_sum = [&](int tree_index, int tree_left, int tree_right, int i, int j) -> int {
-            if (tree_left > j || tree_right < i) {
-                return 0;
-            }
-
-            if (tree_left >= i && tree_right <= j) {
-                return segment_tree[tree_index];
-            }
-
-            int m = (tree_left + tree_right) / 2;
-            return query_range_sum(2 * tree_index + 1, tree_left, m, i, j) +
-                   query_range_sum(2 * tree_index + 2, m + 1, tree_right, i, j);
-        };
-
-        int offset = 10000;
-
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            int num = nums[i];
-            result[i] = query_range_sum(0, 0, num_buckets - 1, 0, (num + offset) - 1);
-            update(0, num + offset, 0, num_buckets - 1);
+bool check(int i,vector<int>&v,int x){
+    int left = i-1;
+    int right = i+1;
+    int count = 0;
+    while(left >= 0 && right <= v.size()-1){
+        if(v[left]  <= v[i] && v[right] >= v[i]){
+            left--;
+            right++;
+            count++;
         }
-
-        return result;
+        else break;
     }
-};
+    if(x<=count) return true;
+    return false;
+}
 
-*/
+int helper(int i,vector<int>&v,int days){
+    int ans = 0;
+    for(int i=0;i<v.size();i++){
+        if(check(i,v,x)) ans++;
+    }
+    return ans;
+}
--- pracatice.cpp
+++ pracatice.cpp
@@ -1,30 +1,51 @@
-
+#include <vector>
+using namespace std;
 
 class Solution {
 public:
-    long long helper(int i,long long p_idx,vector<int>&nums){
-        if(i>=nums.size()) return 0;
-        int take = 0;
-        int not_take = 0;
-        if(isPerfectSquare(p_idx * (i+1))) take = nums[i] + helper(i+1,p_idx * (i+1),nums),cout<<"take when index is : "<<i<<" "<<take<<endl;
-        not_take = helper(i+1,p_idx,nums);
-        cout<<"not take when index is : "<<i<<" "<<not_take<<endl;
-        return max(take,not_take);
-    }
-    bool isPerfectSquare(long long num){
-        long long sr = sqrt(num);
-        if(num == sr*sr) return true;
-        return false;
-    }
-    long long maximumSum(vector<int>& nums) {
-        //calcualae the number of subset with product of indices is a perfect square
-        return helper(0,1,nums);
-        
-        //functionn to calulate a perfect square.
+    vector<int> countSmallerSegmentTree(vector<int>& nums) {
+        int num_buckets = 2 * 10000 + 1;
+        vector<int> segment_tree(4 * num_buckets, 0);
+        vector<int> result(nums.size());
+
+        function<void(int, int, int, int)> update = [&](int tree_index, int bucket, int tree_left, int tree_right) {
+            if (tree_left == tree_right) {
+                segment_tree[tree_index]++;
+                return;
+            }
+
+            int m = (tree_left + tree_right) / 2;
+            if (bucket <= m) {
+                update(2 * tree_index + 1, bucket, tree_left, m);
+            } else if (bucket > m) {
+                update(2 * tree_index + 2, bucket, m + 1, tree_right);
+            }
+
+            segment_tree[tree_index]++;
+        };
+
+        function<int(int, int, int, int)> query_range_sum = [&](int tree_index, int tree_left, int tree_right, int i, int j) -> int {
+            if (tree_left > j || tree_right < i) {
+                return 0;
+            }
+
+            if (tree_left >= i && tree_right <= j) {
+                return segment_tree[tree_index];
+            }
+
+            int m = (tree_left + tree_right) / 2;
+            return query_range_sum(2 * tree_index + 1, tree_left, m, i, j) +
+                   query_range_sum(2 * tree_index + 2, m + 1, tree_right, i, j);
+        };
+
+        int offset = 10000;
+
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            int num = nums[i];
+            result[i] = query_range_sum(0, 0, num_buckets - 1, 0, (num + offset) - 1);
+            update(0, num + offset, 0, num_buckets - 1);
+        }
+
+        return result;
     }
 };
-
-int main(){
-
-    return 0;
-}
